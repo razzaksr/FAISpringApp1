1baeff2e3888ed3d5340ffa57ebffdb765570686
razzaksr <razzaksr@gmail.com>
2022-06-24 16:09:34 +0530

    BAck end created

---
+++ src/main/java/fai/backend/BackEndFAI/BackEndFaiApplication.java
@@ -0,0 +1,13 @@
+package fai.backend.BackEndFAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndFaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndFaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fai/backend/BackEndFAI/BackEndFaiApplicationTests.java
@@ -0,0 +1,13 @@
+package fai.backend.BackEndFAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndFaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
